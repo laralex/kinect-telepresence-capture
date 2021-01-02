48681f058adfd6c3e29598a70ff056c90bafb84a
Alex Larionov <alxs.larionov@gmail.com>
2021-01-03 01:02:42 +0300

    Add spdlog library

--- apps/daemon/Main.cpp
+++ apps/daemon/Main.cpp
@@ -1,12 +1,14 @@
 #include "network/Packet.hpp"
+#include <spdlog/spdlog.h>
 #include <iostream>
 
 int main(int argc, char** argv) {
-    std::cout << argc << ' ' << argv[0] << std::endl;
+    spdlog::info(argc);
+    spdlog::info(argv[0]);
     
     using namespace ktp::network;
     Packet p;
     p.set_data(42+argc);
-    
+
     return 0;
 }
