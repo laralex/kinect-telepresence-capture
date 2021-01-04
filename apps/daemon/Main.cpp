0941699f0ea22c9ded4641635f82567d61de64dd
Alex Larionov <alxs.larionov@gmail.com>
2021-01-05 01:16:34 +0300

    Make daemon use Kinect SDK

--- apps/daemon/Main.cpp
+++ apps/daemon/Main.cpp
@@ -1,11 +1,14 @@
 #include "network/Packet.hpp"
+#include "camera/Kinect.hpp"
 #include <spdlog/spdlog.h>
 #include <iostream>
+
+using namespace ktp;
 
 int main(int argc, char** argv) {
     spdlog::info(argc);
     spdlog::info(argv[0]);
-    
+    camera::KinectTest();
     using namespace ktp::network;
     Packet p;
     p.set_data(42+argc);
