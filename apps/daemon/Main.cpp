5d8c9dd36b32efe3ff6f75310d7bf47f6026a319
Alex Larionov <alxs.larionov@gmail.com>
2021-02-11 22:42:18 +0300

    Add 'cxxopts' library for parsing CLI arguments

--- apps/daemon/Main.cpp
+++ apps/daemon/Main.cpp
@@ -1,13 +1,30 @@
 #include "camera/Kinect.hpp"
 #include <spdlog/spdlog.h>
+#include <cxxopts.hpp>
 #include <iostream>
 
 using namespace ktp;
 
 int main(int argc, char** argv) {
-    spdlog::info(argc);
-    spdlog::info(argv[0]);
-    camera::KinectTest();
+  cxxopts::Options options("test", "A brief description");
 
+  options.add_options()
+    ("p,playback", "Playback file", cxxopts::value<std::string>())
+    ("h,help", "Print usage");
+
+  auto result = options.parse(argc, argv);
+
+  if (result.count("help")) {
+    spdlog::info(options.help());
     return 0;
+  }
+
+  std::string playback_filepath;
+  if (result.count("playback")){
+    playback_filepath = result["playback"].as<std::string>();
+    spdlog::info("Recognized playback file: {}", playback_filepath);
+  }
+
+  camera::KinectTest();
+  return 0;
 }
